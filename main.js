d242bbc2d16a6c7d4f05e5be148916e8df290463
miffy24 <chenzhutao125@163.com>
2018-02-19 21:24:22 +0800

    update

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ myButton.addEventListener('click',function(){
       if(request.status >= 200&&request.status<=300){
         console.log('请求成功')
 	console.log(request.responseText)
+	let string = request.responseText
+	//把符合的json语法的字符串转换成js对应的值
+	let object = window.JSON.parse(string)
+	console.log(typeof object)
+	console.log(object)
+	console.log('object,note')
+	console.log(object.note)
       }else if(request.status >=400){
         console.log('请求失败')
       }
