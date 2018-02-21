965773ab34844642e582f7d738e81d386d7f367d
miffy24 <chenzhutao125@163.com>
2018-02-21 21:24:53 +0800

    简陋的ajax

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
-myButton.addEventListener('click', (e)=>{
-  let request = new XMLHttpRequest()
-  request.open('post', '/xxx') // 配置request
-  request.setRequestHeader('frank','18')
-  request.setRequestHeader('Content-Type','x-www-form-urlencoded')
-  request.send('我偏要设置request第四部分')
-  request.onreadystatechange = ()=>{
-    if(request.readyState === 4){
-      console.log('请求响应都完毕了')
-      console.log(request.status)
-      console.log(request.statusText)
-      if(request.status >= 200 && request.status < 300){
-        console.log('说明请求成功')
-        console.log(request.responseText)
-        let string = request.responseText
-        // 把符合 JSON 语法的字符串
-        // 转换成 JS 对应的值
-        let object = window.JSON.parse(string) 
-        // JSON.parse 是浏览器提供的
-        console.log(typeof object)
-        console.log(object)
-        console.log('object.note')
-        console.log(object.note)
+window.jQuery = function(nodeOrSelector){
+  let nodes ={}
+  nodes.addClass = function(){}
+  nodes.html =function(){}
+  return nodes
+}
 
-      }else if(request.status >= 400){
-        console.log('说明请求失败') 
+window.jQuery.ajax = function(url,method,body,successFn,failFn){
+
+  let request = new XMLHttpRequest() //实例化XMLHttpRequest对象
+  request.open(method, url)//配置request 
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status <= 300) {
+        successFn.call(undefined, request.responseText)
+      } else if (request.status >= 400)  {
+        failFn.call(undefined, request)
       }
-
     }
   }
+  request.send(body)
+}
+
+
+myButton.addEventListener('click', (e)=>{
+  window.jQuery.ajax(
+	  '/xxx',
+	  'post',
+	  'a=1&b=2',
+	  (responseText)=>{console.log(1)},
+	  (request)=>{console.log(2)}
+   )
 })
