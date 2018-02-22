c90728719a457251d150b7d9ffc181abb52fd36f
miffy24 <chenzhutao125@163.com>
2018-02-22 13:50:29 +0800

    promise 标准化操作

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-function success(responseText){
-  console.log(responseText)
-}
-function fail(responseText){
-  console.log(request)
-}
-
 myButton.addEventListener('click', (e)=>{
   $.ajax({
       url: '/xxx',
       method: 'get',
-  }).then(success,fail)
+  }).then(
+    (responseText)=>{console.log(responseText)},
+    (request)=>{console.log('error')}
+  
+  )
 })
